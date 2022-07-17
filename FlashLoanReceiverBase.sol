84a80bcd0a926c816c7058dac02c914aa1480629
nikaluka <47041738+nikaluka@users.noreply.github.com>
2022-07-17 18:35:35 +0200

    Update FlashLoanReceiverBase.sol

--- FlashLoanReceiverBase.sol
+++ FlashLoanReceiverBase.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.6.12;
 
 import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "../Interfaces.sol";
-import { SafeERC20, SafeMath } from "Libraries.sol";
+import { SafeERC20, SafeMath } from "../Libraries.sol";
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
